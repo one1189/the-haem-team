c9d59e15d723d68d3bb88b6bc052700fd1972dba
codeOne11 <ca.watton111@gmail.com>
2024-01-11 12:39:55 +0000

    Add ability to approve comments

--- support_site/admin.py
+++ support_site/admin.py
@@ -18,4 +18,8 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
+    actions = ('approved_comments')
+
+    def approved_comments(self, request, queryset):
+        queryset.update(approved=True)
 
